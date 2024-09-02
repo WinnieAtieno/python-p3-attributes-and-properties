980f04dac5e5d332bcc6aa1016a125608b936d3f
WinnieAtieno <winnieotieno773@gmail.com>
2024-09-02 22:58:19 +0300

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
+
+
+
+class Dog:
+    # List of approved breeds
+    APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -10,6 +15,35 @@ APPROVED_BREEDS = [
     "Pug",
     "Pointer"
 ]
+ 
+    def __init__(self, name = None, breed=None):
+        if name is not None:
+            self.set_name(name)
+        else:
+            self.name = None
+        
+        if breed is not None:
+            self.set_breed(breed)
+        else:
+            self.breed = None
 
-class Dog:
-    pass
+    def set_name(self, name):
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            self.name = None
+        else:
+            self.name = name
+
+    def set_breed(self, breed):
+        if breed not in self.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            self.breed = None
+        else:
+            self.breed = breed
+
+    
+fido = Dog("Fido")
+print(fido.name)
+      
+   
+
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -14,6 +15,25 @@ APPROVED_JOBS = [
     "Marketing",
     "Purchasing"
 ]
+    def __init__(self, name = None, job=None):
+        self.name = None
+        self.job = None
+        if name is not None:
+            self.set_name(name)
+        if job is not None:
+            self.set_job(job)
 
-class Person:
-    pass
+    def set_name(self, name):
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()
+
+    def set_job(self, job):
+        if job not in Person.APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job
+   
+guido = Person("Guido","ITC")
+print(guido.name)
